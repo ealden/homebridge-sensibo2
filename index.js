3313308d835b6e5ded1346eb9975bfbe44155533
Ealden Esto E. Escañan <ealden@escanan.com>
2018-08-11 14:06:12 +0800

    Get or set fan status

--- index.js
+++ index.js
@@ -8,8 +8,33 @@ module.exports = function(homebridge) {
 }
 
 function SensiboAccessory(log, config) {
+  this.log = log;
+  this.name = config['name'];
+
+  this.on = false;
+
+  let fanService = new Service.Fan(this.name);
+  fanService.getCharacteristic(Characteristic.On)
+    .on('get', this.getFanStatus.bind(this))
+    .on('set', this.setFanStatus.bind(this));
+
+  this.fanService = fanService;
 }
 
 SensiboAccessory.prototype.getServices = function() {
-  return [];
+  return [this.fanService];
+}
+
+SensiboAccessory.prototype.getFanStatus = function(next) {
+  this.log('Get Fan Status: ' + this.on);
+
+  return next(null, this.on);
+}
+
+SensiboAccessory.prototype.setFanStatus = function(on, next) {
+  this.log('Set Fan Status: ' + on);
+
+  this.on = on;
+
+  return next();
 }
